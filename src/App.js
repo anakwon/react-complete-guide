30e162f344eb97ee1dfaf461ac0eaa6c2c7fa9e5
Ana Kwon <akwon@arbonne.com>
2018-08-07 15:50:48 -0700

    commits for REACT complete guide

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+  state = {
+    persons : [
+      { name : "Max", age : 29 },
+      { name : "Manu", age : 30 },
+      { name : "Stephanie", age : 26 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!'); 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I'm a React App</h1>
+        <p>Is this really working?</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
+      </div>
+    );
+    
+    //return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'Does this work now??'));
+  }
+}
+
+export default App;
