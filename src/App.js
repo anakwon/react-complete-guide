4bd1cfc6b1ad59b9fe3b60e621a19e24ccc00ad1
Ana Kwon <akwon@arbonne.com>
2018-08-08 09:40:04 -0700

    passing parameter using functions

--- src/App.js
+++ src/App.js
@@ -12,18 +12,47 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('Was clicked!'); 
+    //DON'T DO THIS 
+    //this.state.persons[0] = "Anna"
+
+    this.setState({
+      persons : [
+        { name : newName, age : 25 },
+        { name : "Manu", age : 30 },
+        { name : "Stephanie", age : 32 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons : [
+        { name : "Max", age : 25 },
+        { name : event.target.value, age : 30 },
+        { name : "Stephanie", age : 32 }
+      ]
+    })
   }
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>Is this really working?</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Annnnna')}>Switch Name</button>
+        <Person 
+          name = {this.state.persons[0].name} 
+          age = {this.state.persons[0].age} />
+        <Person 
+          name = {this.state.persons[1].name} 
+          age = {this.state.persons[1].age}
+          click = {this.switchNameHandler.bind(this, 'Anna')}
+          changed = {this.nameChangedHandler}
+          >My Hobbies: Racing</Person>
+        <Person 
+          name = {this.state.persons[2].name} 
+          age = {this.state.persons[2].age}/>
       </div>
     );
     
